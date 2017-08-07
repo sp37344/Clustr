93ff337cc22de217cc083f43be08d8dbfed5eec2
ADELLE INGRID DIMITUI <adimitui@princeton.edu>
2017-08-07 04:31:19 -0400

    finished visuals for login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { HomePage } from '../home/home';
+
 @Component({
+	selector: 'page-login',
 	templateUrl: 'login.html'
 })
 
 export class LoginPage {
 	constructor(public nav: NavController) {}
+
+	logIn() {
+		this.nav.push(HomePage);
+	}
+
+	startTutorial() {
+		// Do nothing
+	}
 }
