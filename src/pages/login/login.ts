8b5d2be416e24f5c66b4fdfc38b619d638dc7dec
ADELLE INGRID DIMITUI <adimitui@princeton.edu>
2017-08-07 02:38:15 -0400

    added login page and set login page as initial root

---
+++ src/pages/login/login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+	templateUrl: 'login.html'
+})
+
+export class LoginPage {
+	constructor(public nav: NavController) {}
+}
